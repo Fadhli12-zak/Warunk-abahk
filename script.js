352e9a3b5f314ad17d0a8e458a0859d22c2b8347
fadhlidzaki <fadhlidzaki120@gmail.com>
2023-10-20 14:21:58 +0700

    warunk abahk

---
+++ script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// while click hamburger menu
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// click out the sidebar for vanish nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
